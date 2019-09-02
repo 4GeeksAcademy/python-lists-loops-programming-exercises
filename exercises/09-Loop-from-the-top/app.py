fe0b7287104f393c19d2b6ed9cc224b083bfc0ed
ESPINO <espvtes@gmail.com>
2019-09-02 17:26:49 +0000

    flip list

--- exercises/09-Loop-from-the-top/app.py
+++ exercises/09-Loop-from-the-top/app.py
@@ -1,3 +1,12 @@
 my_sample_list = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12]
 
 #nothing change above, the magic pass below:
+
+
+for i in range(len(my_sample_list)):
+    print(my_sample_list[-i-1])
+
+
+for i in reversed(my_sample_list):
+    print(i)
+
