197c627c7a08c486acd3ad1e838d6a0eaf6f45d4
ESPINO <espvtes@gmail.com>
2019-09-02 16:18:14 +0000

    def function assign random number

--- exercises/05-Print-the-last-one/app.py
+++ exercises/05-Print-the-last-one/app.py
@@ -1,8 +1,24 @@
 #You have to import random function:
 import random
 #Feel happy to write the code below, good luck:
-my_stupid_list = []
-for i in range(1, 100):
-    my_stupid_list.append(random.randint(1,100))
-    the_last_one = my_stupid_list[-1]
+
+def generate_random_list():
+    aux_list = []
+    randonlength = random.randint(1, 100)
+
+    for i in range(randonlength):
+        aux_list.append(randonlength)
+        i += i
+    return aux_list
+
+my_stupid_list = generate_random_list()
+the_last_one = my_stupid_list[-1]
+
 print(the_last_one)
+
+
+# my_stupid_list = []
+# for i in range(1, 100):
+#     my_stupid_list.append(random.randint(1,100))
+#     the_last_one = my_stupid_list[-1]
+# print(the_last_one)
