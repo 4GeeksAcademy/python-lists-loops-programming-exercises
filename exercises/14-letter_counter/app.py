e904e75b408896c335e25e156d0b01c25d3debc1
ESPINO <espvtes@gmail.com>
2019-09-02 23:34:43 +0000

    counter letter

---
+++ exercises/14-letter_counter/app.py
@@ -0,0 +1,18 @@
+par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
+
+#Your code go here:
+
+count = {}
+for i in par:
+    if i in count:
+        count[i] += 1
+    else:
+        count[i] = 1
+print(count)
+
+
+letter = {}
+for i in par:
+    letter = letter.get(i, 0) + 1
+
+print(letter)
