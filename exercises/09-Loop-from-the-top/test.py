e74ad3bc8abd6a721717c01c7c715ab723e5fb03
ESPINO <espvtes@gmail.com>
2019-09-02 17:38:55 +0000

    looping 2 by 2

--- exercises/09-Loop-from-the-top/test.py
+++ exercises/09-Loop-from-the-top/test.py
@@ -4,13 +4,3 @@ sys.stdout = buffer = io.StringIO()
 
 from app import my_sample_list
 import pytest
-
-@pytest.mark.it('Try looping from the end to the beginning')
-def test_loop():
-    for i in reversed(my_sample_list):
-        print(i)
-
-
-@pytest.mark.it("other way to flip list in python")
-def test_print():
-    print(my_sample_list[::-1])
--- exercises/10-Loop-adding-two/app.py
+++ exercises/10-Loop-adding-two/app.py
@@ -1,3 +1,11 @@
 my_sample_list= [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12]
 
 #your code go below of this line, nothing change above:
+
+
+for i in range(0, len(my_sample_list), 2):
+    print(my_sample_list[i])
+
+
+for i in my_sample_list[0::2]:
+    print(i)
