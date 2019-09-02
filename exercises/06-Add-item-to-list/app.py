298deb2fa3957b1329a4885328cbc2694d386ae7
ESPINO <espvtes@gmail.com>
2019-09-02 16:22:56 +0000

    add random number to the list

--- exercises/06-Add-item-to-list/app.py
+++ exercises/06-Add-item-to-list/app.py
@@ -1,6 +1,9 @@
+#Remember import random function here:
 import random
 
 list = [4,5,734,43,45]
 #Your code here:
 
-
+for i in range(1, 11):
+    list.append(random.randint(1, 10))
+print(list)
--- exercises/06-Add-item-to-list/test.py
+++ exercises/06-Add-item-to-list/test.py
@@ -9,6 +9,4 @@ import random
 
 @pytest.mark.it("add ten random number to the list")
 def test_random():
-    for i in range(1, 11):
-        list.append(random.randint(1, 10))
-print(list)
+
