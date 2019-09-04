f2b05345dc7dc8e3da0a30720a294233e2b9927b
ESPINO <espvtes@gmail.com>
2019-09-04 22:24:14 +0000

    25 divide and conquer

---
+++ exercises/25-Divide_and_conquer/app.py
@@ -0,0 +1,17 @@
+list_of_numbers = [4,	80,	85,	59,	37,25,	5,	64,	66,	81,20,	64,	41,	22,	76,76,	55,	96,	2,	68]
+
+
+#Your code here:
+
+
+# odd_number = []
+# even_number = []
+# def merge_two_list(items):
+#     for i in items:
+#         if i % 2 != 0:
+#             odd_number.append(i)
+#             i += i
+#         else:
+#             even_number.append(i)
+#     return odd_number + even_number
+print(merge_two_list(list_of_numbers))
