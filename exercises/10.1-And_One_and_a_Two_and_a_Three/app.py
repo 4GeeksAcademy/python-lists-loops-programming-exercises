e1eb9cd83376ce82d4489d508a1d0adab386fdb0
ESPINO <espvtes@gmail.com>
2019-09-09 23:20:25 +0000

    loop dict iteration

--- exercises/10.1-And_One_and_a_Two_and_a_Three/app.py
+++ exercises/10.1-And_One_and_a_Two_and_a_Three/app.py
@@ -6,6 +6,20 @@ contact = {
 #Your code here:
 
 # declare and aux variable and assign the value of the key
+k = contact.keys()
 
 
 # loop the entire dict and concactenate the items in one output
+for k, v in contact.items():
+    print("{0}: {1}".format(k, v))
+
+
+#another form:
+x = contact.keys()
+
+for x, e in contact.items():
+    print(x, ":", e)
+
+#loop without declaration:
+for key, value in contact.items():
+    print(key, ":", value)
