811d6e99232efbb9ebf7b994584cfd15771522aa
ESPINO <espvtes@gmail.com>
2019-09-11 23:51:21 +0000

    functions random numbers

---
+++ exercises/12.4-Map_list_of_objects/app.py
@@ -0,0 +1,9 @@
+people = [
+	{ "name": 'Joe', "birthDate": (1986,10,24) },
+	{ "name": 'Bob', "birthDate": (1975,5,24) },
+	{ "name": 'Erika', "birthDate": (1989,6,12) },
+	{ "name": 'Dylan', "birthDate": (1999,12,14) },
+	{ "name": 'Steve', "birthDate": (2003,4,24) }
+]
+
+
---
+++ exercises/14-Matrix_Builder/app.py
@@ -0,0 +1,13 @@
+import random
+
+def matrix_function(numbers):
+    row = []
+    columns = []
+    for i in range(numbers):
+        i+=i
+        for x in range(numbers):
+            x+=i
+            columns.append(random.randint(0,1)*1)
+        row.append(columns)
+        return row
+print(matrix_function(5))
