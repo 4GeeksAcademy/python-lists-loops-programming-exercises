98c41027ddfbdd2c3bff90d5af17789db10c2ed8
ESPINO <espvtes@gmail.com>
2019-09-12 22:27:15 +0000

    matrix builder

--- exercises/14-Matrix_Builder/app.py
+++ exercises/14-Matrix_Builder/app.py
@@ -2,15 +2,15 @@ import random
 
 def matrix_function(numbers):
     row = []
-    columns = []
     for i in range(numbers):
         i+=i
+        columns = []
         for x in range(numbers):
             x+=i
             columns.append(random.randint(0,1)*1)
         row.append(columns)
-        return row
+    return row
 print(matrix_function(5))
 
-#this in onle returning the 5 integer numbers
+#this in onle returning the 5 integer numbers into the rows and columns
 # have to reviewing this
