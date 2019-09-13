63e7c302e8092fc6065e4c9d1019491e2cc28511
ESPINO <espvtes@gmail.com>
2019-09-13 00:58:33 +0000

    pytest 2.4

--- exercises/02.4-One_last_looping/app.py
+++ exercises/02.4-One_last_looping/app.py
@@ -2,16 +2,12 @@ my_sample_list = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','Fern
 
 #Your code here:
 
-my_sample_list[1] == "Steve"
-my_sample_list[-1] == "Pepe"
-my_sample_list[0] == my_sample_list[2] + my_sample_list[4]
-
-
-for i in reversed(my_sample_list):
-    print(i)
+my_sample_list[1] = "Steve"
+my_sample_list[-1] = "Pepe"
+my_sample_list[0] = my_sample_list[2] + my_sample_list[4]
 
 
 new_list = []
-for i in range(len(my_sample_list)):
-    new_list.append(my_sample_list[-i-1])
-print(new_list)
+for names in range(len(my_sample_list)):
+    new_list.append(my_sample_list[-names-1])
+print('\n'.join(new_list))
