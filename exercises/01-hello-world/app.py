795eeafa86e4333b8d4bcedc8916dfb2ea6d656c
ESPINO <espvtes@gmail.com>
2019-09-18 23:01:28 +0000

    pytest

--- exercises/01-hello-world/app.py
+++ exercises/01-hello-world/app.py
@@ -1,6 +1,3 @@
 
 
 #You have to print `hello` in the console, your code go here:
-
-
-print("Hello World")
--- exercises/05-Sum_all_items/app.py
+++ exercises/05-Sum_all_items/app.py
@@ -5,8 +5,7 @@ def sum_all_values(items):
 
     total= 0
     #The magic happens here:
-    for x in my_sample_list:
-        total = total + x
+   
 
     return total
 print(sum_all_values(my_sample_list))
