6667dc958f5c48efc4bcb2b8bea75a3743082d97
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-03-31 23:02:10 +0000

    updated exercise 02-loop-list

--- exercises/02-Loop-list/test.py
+++ exercises/02-Loop-list/test.py
@@ -1,17 +1,11 @@
-import io
-import sys
-sys.stdout =  buffer = io.StringIO()
-
-
-
-import pytest
-import app
-import os
+import io, sys, pytest, os, re
+path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
 
 @pytest.mark.it("Print the all items from the list")
-def test_output():
-    captured = buffer.getvalue()
-    assert "232\n32\n1\n4\n55\n4\n3\n32\n3\n24\n5\n5\n5\n34\n2\n35\n5365743\n52\n34\n3\n55\n" in captured
+def test_output(capsys, app):
+    app()
+    captured = capsys.readouterr()
+    assert "232\n32\n1\n4\n55\n4\n3\n32\n3\n24\n5\n5\n5\n34\n2\n35\n5365743\n52\n34\n3\n55\n" in captured.out
 
 @pytest.mark.it("Be sure that you use the for loop in the exercises")
 def test_use_forLoop():
