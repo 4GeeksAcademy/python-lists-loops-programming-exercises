7fe4eec6b63ec6c0d5a6db41bf2082ad1fea1a63
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-03-31 23:21:14 +0000

    update exercise 02.2-Loop-adding-two

--- exercises/02.2-Loop-adding-two/test.py
+++ exercises/02.2-Loop-adding-two/test.py
@@ -1,23 +1,17 @@
-import io
-import sys
-sys.stdout = buffer = io.StringIO()
-
-from app import my_sample_list
-import pytest
-import app
-import os
+import io, sys, pytest, os
+path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
 
 @pytest.mark.it("Loop two by two")
-def test_output():
-    captured = buffer.getvalue()
-    assert "3423\n4\n654\n867543\n48\n55\n25\n" in captured
+def test_output(capsys, app):
+    app()
+    captured = capsys.readouterr()
+    assert "3423\n4\n654\n867543\n48\n55\n25\n" in captured.out
 
 
-@pytest.mark.it("The for loop was used")
-def test_use_for():
-    captured = buffer.getvalue()
+@pytest.mark.it("Be sure that you use the for loop in the exercises")
+def test_use_forLoop():
 
-    f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
+    f = open(path)
     content = f.read()
     assert content.find("for") > 0
 
