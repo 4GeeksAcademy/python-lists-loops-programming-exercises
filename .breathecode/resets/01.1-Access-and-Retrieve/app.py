f0e41335691515e89a7049a7c32567da56d600cf
Alejandro Sanchez <aalejo@gmail.com>
2020-04-18 23:01:08 +0000

    moved the counter letters towars the end

--- .breathecode/resets/01.1-Access-and-Retrieve/app.py
+++
@@ -1,8 +0,0 @@
-
-my_list = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
-
-# 1. print the item here
-
-# 2. change the position were 'thursday' is to None
-
-# 3. print that position now here
