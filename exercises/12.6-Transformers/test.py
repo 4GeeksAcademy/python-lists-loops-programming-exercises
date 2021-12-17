9ca2689937caecc9b2f33c0238fe5b10e6c28880
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2021-12-17 15:27:48 +0000

    Exercise 12.6-Transformers modified, readme's, app.py and test improved

--- exercises/12.6-Transformers/test.py
+++ exercises/12.6-Transformers/test.py
@@ -1,29 +1,44 @@
 import io, sys, pytest, os, re
 path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
 
+@pytest.mark.it("You should create a function named data_transformer")
+def test_function_exists(app):
+    try:
+        app.data_transformer
+    except AttributeError:
+        raise AttributeError("The function 'data_transformer' should exist on app.py")
 
-@pytest.mark.it("Nice work!!! the transformed_data variable is passed")
-def test_output(capsys, app):
-    import app
-    captured = capsys.readouterr()
-    assert "['Mario Montes', 'Joe Biden', 'Bill Clon', 'Hilary Mccafee', 'Bobby Mc birth']\n" in captured.out
+@pytest.mark.it('The function named data_transformer should return the correct value')
+def test_function_value1(app):
+    try:
+        incoming_ajax_data = [
+	        { "name": 'Mario', "last_name": 'Montes' },
+	        { "name": 'Joe', "last_name": 'Biden' },
+	        { "name": 'Bill', "last_name": 'Clon' },
+	        { "name": 'Hilary', "last_name": 'Mccafee' },
+	        { "name": 'Bobby', "last_name": 'Mc birth' }
+        ]
 
-@pytest.mark.it("Using map function")
-def test_map():
+        assert app.data_transformer(incoming_ajax_data) == ['Mario Montes', 'Joe Biden', 'Bill Clon', 'Hilary Mccafee', 'Bobby Mc birth']
+    except AttributeError:
+        raise AttributeError("The variable 'transformed_data' should have the correct value")
+
+@pytest.mark.it('The function named data_transformer should return the correct value (Testing with a different list)')
+def test_function_value2(app):
+    try:
+        incoming_ajax_data = [
+	        { "name": 'Donald', "last_name": 'Trump' },
+	        { "name": 'Cristiano', "last_name": 'Ronaldo' },
+	        { "name": 'Lebron', "last_name": 'James' },
+	        { "name": 'Steve', "last_name": 'Jobs' }
+        ]
+
+        assert app.data_transformer(incoming_ajax_data) == ['Donald Trump', 'Cristiano Ronaldo', 'Lebron James', 'Steve Jobs']
+    except AttributeError:
+        raise AttributeError("The variable 'transformed_data' should have the correct value")
+
+@pytest.mark.it("You should use the map method")
+def test_map_usage():
     f = open(os.path.dirname(os.path.abspath(__file__)) + '/app.py')
     content = f.read()
     assert content.find("map") > 0
-
-@pytest.mark.it("Create the function my_var")
-def test_function_exists(app):
-    try:
-        app.my_var
-    except AttributeError:
-        raise AttributeError("The function 'my_var' should exist on app.py")
-
-@pytest.mark.it("Create the variable transformedData")
-def test_variable_exists(app):
-    try:
-        app.transformedData
-    except AttributeError:
-        raise AttributeError("The variable 'transformedData' should exist on app.py")
