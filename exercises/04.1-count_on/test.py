fdc92bccd286bf979e491d47d5c5bfe3c5ee1277
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2022-01-18 16:36:00 -0400

    Update test.py

--- exercises/04.1-count_on/test.py
+++ exercises/04.1-count_on/test.py
@@ -1,23 +1,22 @@
 import io, sys, os, pytest, re
 path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
 
-@pytest.mark.it("Cool !!! 😎 You have the all data-type object")
+@pytest.mark.it("You have to print the value of the variable 'hello' in the console")
 def test_all_data_type(capsys, app):
     app()
     captured = capsys.readouterr()
     assert "[[2, 1], {'name': 'juan'}]\n" in captured.out
 
-@pytest.mark.it("Use the for loop")
+@pytest.mark.it("You have to use a for loop")
 def test_for_loop():
     with open(path, 'r') as content_file:
         content = content_file.read()
         regex = re.compile(r"for(\s)")
         assert bool(regex.search(content)) == True
 
-@pytest.mark.it("Use if statement")
+@pytest.mark.it("You have to use an if statement")
 def test_if():
     with open(path, 'r') as content_file:
         content = content_file.read()
         regex = re.compile(r"if(\s)")
         assert bool(regex.search(content)) == True
-
