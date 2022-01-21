54302a6e6d692d5403654cd8bec3fe135dd1f418
Martín Suárez <ms.prgramador@gmail.com>
2022-01-21 18:00:22 +0000

    done

--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -3,5 +3,5 @@ my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
 
 #your code go here:
-
+hello = [[2, 1], {'name': 'juan'}]
 print(hello)
--- exercises/04.1-count_on/test.py
+++ exercises/04.1-count_on/test.py
@@ -1,11 +1,14 @@
 import io, sys, os, pytest, re
 path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
 
-@pytest.mark.it("You have to print the value of the variable 'hello' in the console")
-def test_all_data_type(capsys, app):
-    app()
-    captured = capsys.readouterr()
-    assert "[[2, 1], {'name': 'juan'}]\n" in captured.out
+
+@pytest.mark.it("The variable `hello` should exist")
+def test_for_loop():
+    with open(path, 'r') as content_file:
+        content = content_file.read()
+        regex = re.compile(r"hello")
+        assert bool(regex.search(content)) == True
+
 
 @pytest.mark.it("You have to use a for loop")
 def test_for_loop():
@@ -20,3 +23,9 @@ def test_if():
         content = content_file.read()
         regex = re.compile(r"if(\s)")
         assert bool(regex.search(content)) == True
+
+@pytest.mark.it("You have to print the value of the variable 'hello' in the console")
+def test_all_data_type(capsys, app):
+    app()
+    captured = capsys.readouterr()
+    assert "[[2, 1], {'name': 'juan'}]\n" in captured.out
