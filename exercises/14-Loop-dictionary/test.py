6e510b5c521e901b847b1a69157c9b33f1820bda
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2022-01-24 16:46:15 -0400

    Update test.py

--- exercises/14-Loop-dictionary/test.py
+++ exercises/14-Loop-dictionary/test.py
@@ -5,21 +5,21 @@ path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
 def test_love():
     f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
     content = f.read()
-    regex = re.compile(r"spanish_translations\[\s*[\'\"]\s*love\s*[\'\"]\s*\]\s*\=\s*[\'\"]\s*amor\s*[\'\"]")
+    regex = re.compile(r"spanish_translations\[\s*[\'\"]love[\'\"]\s*\]\s*\=\s*[\'\"]amor[\'\"]")
     assert bool(regex.search(content)) == True
 
 @pytest.mark.it("You should add `code` programatically to the dictionary")
 def test_code():
     f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
     content = f.read()
-    regex = re.compile(r"spanish_translations\[\s*[\'\"]\s*code\s*[\'\"]\s*\]\s*\=\s*[\'\"]\s*codigo\s*[\'\"]")
+    regex = re.compile(r"spanish_translations\[\s*[\'\"]code[\'\"]\s*\]\s*\=\s*[\'\"]codigo[\'\"]")
     assert bool(regex.search(content)) == True
 
 @pytest.mark.it("You should add `smart` programatically to the dictionary")
 def test_smart():
     f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
     content = f.read()
-    regex = re.compile(r"spanish_translations\[\s*[\'\"]\s*smart\s*[\'\"]\s*\]\s*\=\s*[\'\"]\s*inteligente\s*[\'\"]")
+    regex = re.compile(r"spanish_translations\[\s*[\'\"]smart[\'\"]\s*\]\s*\=\s*[\'\"]inteligente[\'\"]")
     assert bool(regex.search(content)) == True
 
 @pytest.mark.it("You should print in the console the dictionary with proper translations")
