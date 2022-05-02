247325fe156aa2ee2f19b37c4380ce3361967bdf
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2022-05-02 21:43:30 +0000

    solution.hide.py and test improved

---
+++ exercises/15.2-Parking_lot_check/solution.hide.py
@@ -0,0 +1,20 @@
+parking_state = [
+  [1,1,1],
+  [0,0,0],
+  [1,1,2]
+]
+
+#Your code go here:
+def get_parking_lot(matrix):
+  state = {'total_slots': 0, 'available_slots': 0, 'occupied_slots': 0}
+  for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+      if matrix[i][j] == 1:
+        state["occupied_slots"] += 1
+        state["total_slots"] += 1
+      elif matrix[i][j] == 2:
+        state["available_slots"] += 1
+        state["total_slots"] += 1
+  return state
+
+get_parking_lot(parking_state)
--- exercises/15.2-Parking_lot_check/test.py
+++ exercises/15.2-Parking_lot_check/test.py
@@ -9,9 +9,18 @@ def test_variable_exists(app):
         raise AttributeError("The function get_parking_lot should exist on app.py")
 
 @pytest.mark.it('The function get_parking_lot should return an object with correct values')
-def test_variable_exists(app):
+def test_correct_output(app):
     value1 = [[1,1,1], [0,0,0], [1,1,2]]
     result1 = {'total_slots': 6, 'available_slots': 1, 'occupied_slots': 5}
+    try:
+        assert app.get_parking_lot(value1) == result1
+    except AttributeError:
+        raise AttributeError("The function get_parking_lot should exist on app.py")
+
+@pytest.mark.it('The function get_parking_lot should return an object with correct values')
+def test_different_values(app):
+    value1 = [[1,2,1,0], [0,1,0,2], [1,0,1,1]]
+    result1 = {'total_slots': 8, 'available_slots': 2, 'occupied_slots': 6}
     try:
         assert app.get_parking_lot(value1) == result1
     except AttributeError:
