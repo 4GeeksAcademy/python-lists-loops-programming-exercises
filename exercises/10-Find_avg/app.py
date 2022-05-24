32b316faf2bb3a0f1c29dab9bb874d60a1c479b6
Sadiel0 <sadielalmanza@outlook.com>
2022-05-24 01:20:10 +0000

    exercise works fine. Also switched to version 1.0.73

--- exercises/10-Find_avg/app.py
+++ exercises/10-Find_avg/app.py
@@ -2,3 +2,10 @@ my_list = [2323,4344,2325,324413,21234,24531,2123,42234,544,456,345,42,5445,23,5
 
 #Your code here:
 
+sum = 0
+count = 0
+for number in my_list:
+    sum += number
+    count += 1
+average = sum / count
+print(average)
