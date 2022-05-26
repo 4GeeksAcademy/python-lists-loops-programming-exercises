33fef50fff7ae370af186f5566f2fbc5dba6aeb9
Sadiel0 <sadielalmanza@outlook.com>
2022-05-26 12:50:06 +0000

    updated readme.es

--- exercises/12.3-Map_data_types/app.py
+++ exercises/12.3-Map_data_types/app.py
@@ -2,7 +2,7 @@ list_Strings = ['1','5','45','34','343','34',6556,323]
 
 
 def type_list(items):
-        return type(items)
+        return items
 
 new_list = list(map(type_list, list_Strings))
 print(new_list)
