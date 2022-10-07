d87007c5e0afc22be71a7e22a968351ea106afdf
dsilva06 <diegosilva06092001@gmail.com>
2022-10-07 14:58:29 +0000

    fixed python test

--- exercises/12.6-Transformers/solution.hide.py
+++ exercises/12.6-Transformers/solution.hide.py
@@ -7,3 +7,7 @@ incoming_ajax_data = [
 ]
 
 #Your code go here:
+def data_transformer(data_list):
+    return list(map(lambda element: f"{element['name']} {element['last_name']}", data_list))
+
+print(data_transformer(incoming_ajax_data))
