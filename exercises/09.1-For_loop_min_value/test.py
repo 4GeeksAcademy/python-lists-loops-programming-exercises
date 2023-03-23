e8766340cc12f693144216fec7adb454877ac621
ErnestoXG <ernestox.061@gmail.com>
2023-03-23 15:15:28 +0000

    updated solution and test accordingly

--- exercises/09.1-For_loop_min_value/test.py
+++ exercises/09.1-For_loop_min_value/test.py
@@ -1,11 +1,36 @@
 import io, sys, os, pytest, re
 path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
 
-@pytest.mark.it("Print the minimum value from the list")
-def test_min(capsys, app):
-    app()
+@pytest.mark.it('The function minInteger must exist')
+def test_function_existence(capsys, app):
+    assert app.minInteger
+
+@pytest.mark.it("The function should return the minimum number from a list")
+def test_output(capsys, app):
+    result = app.minInteger([43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34])
+    assert result == 1
+    
+@pytest.mark.it("The function should print the minimum number from my_list")
+def test_printed_output(capsys, app):
+    def minInteger_wrapper(my_list):
+        result = app.minInteger(my_list)
+        print(result)
+        return result
+
+    original_minInteger = app.minInteger
+    app.minInteger = minInteger_wrapper
+
+    exec(open(app.__file__).read())
     captured = capsys.readouterr()
-    assert "23\n" in captured.out
+
+    app.minInteger = original_minInteger
+
+    assert int(captured.out.strip()) == min(app.my_list)
+
+@pytest.mark.it("The function should work with other lists")
+def test_output_2(capsys, app):
+    result = app.minInteger([43,23,6,8733,43,1,4,6,3,67,8,99999,3,7,5435,63])
+    assert result == 1
 
 @pytest.mark.it("Use the for loop")
 def test_for_loop():
