e2027b9cebefdc8023835f82a91f16691252af06
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-15 07:00:23 +0000

    added test to prevent hard coding

--- exercises/01.2-Retrieve-items/test.py
+++ exercises/01.2-Retrieve-items/test.py
@@ -1,5 +1,6 @@
-import io, sys, pytest
+import io, sys, pytest, re, os
 
+path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
 
 @pytest.mark.it('You have to print the 1st element of the list')
 def test_output_one(capsys, app):
@@ -12,4 +13,11 @@ def test_output_fourd(capsys, app):
     app()
     captured = capsys.readouterr()
     assert  "43\n" in captured.out
+
+@pytest.mark.it('You should not be hard coding any values')
+def test_output_one(capsys, app):
+    with open(path, 'r') as content_file:
+        content = content_file.read()
+        regex = re.compile(r"\(\s*(\"[0-9]*|\'[0-9]*|[0-9]+)\s*")
+        assert bool(regex.search(content)) == False
 
