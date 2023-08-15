7ca8312c34684a5febf3b91e2e8917cf42dff788
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-15 06:01:06 +0000

    added visual cues

--- exercises/01.1-Access-and-Retrieve/app.py
+++ exercises/01.1-Access-and-Retrieve/app.py
@@ -1,8 +1,8 @@
 
 my_list = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
 
-# 1. print the item here
+# 1.✅ ↓ print the item here ↓ ✅
 
-# 2. change the position were 'thursday' is to None
+# 2.✅ ↓ change the position were 'thursday' is to None ↓ ✅
 
-# 3. print that position now here
+# 3.✅ ↓ print that position now here ↓ ✅
