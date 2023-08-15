0adf1744d63fc7a219413efabb0adacfb1cf54ea
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-15 06:37:50 +0000

    test now prevents hard coding

--- exercises/01.1-Access-and-Retrieve/test.py
+++ exercises/01.1-Access-and-Retrieve/test.py
@@ -1,10 +1,22 @@
-import io, sys, pytest
+import io, sys, pytest, os, re
+
+path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
+
 
 @pytest.mark.it('Your console have to print the 3rd item from the `list`')
 def test_output_one(capsys, app):
     app()
     captured = capsys.readouterr()
     assert "tuesday\n" in captured.out
+
+
+@pytest.mark.it('You should not be hard coding any values')
+def test_output_one(capsys, app):
+    with open(path, 'r') as content_file:
+        content = content_file.read()
+        regex = re.compile(r"\(\s*(\"|\')\s*[a-zA-Z]*\s*")
+        assert bool(regex.search(content)) == False
+
 
 @pytest.mark.it('Your code have to print the position of step 2')
 def test_output_two(capsys, app):
