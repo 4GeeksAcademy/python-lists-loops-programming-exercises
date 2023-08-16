f5365c68fa62f9e9e8cd91fd527ba4f25f66c9f7
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-16 05:35:13 +0000

    rebumped test to check against hard coding and be flexible | no loops required

--- exercises/01.4-Add-item-to-list/test.py
+++ exercises/01.4-Add-item-to-list/test.py
@@ -1,32 +1,32 @@
 import io, sys, pytest, os, random, re
 
+path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
+
+
 @pytest.mark.it("Import the random package")
 def test_import_random():
-    path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
     with open(path, 'r') as content_file:
         content = content_file.read()
-        regex = re.compile(r"import(\s)+random")
+        regex = re.compile(r"^[^#]*\bimport(\s)+random\b", re.MULTILINE)
         assert bool(regex.search(content)) == True
 
-@pytest.mark.it("Use the for loop to loop 10 times")
-def test_for_loop():
-    path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
-    with open(path, 'r') as content_file:
-        content = content_file.read()
-        regex = re.compile(r"for(\s)+(\w)+(\s){1,3}in(\s){1,3}range")
-        assert bool(regex.search(content)) == True
-
-
-@pytest.mark.it("Use the randint function to add a radom number to the list each time you loop")
+@pytest.mark.it("Use the randint function to add a random number between 1 and 100 to the list")
 def test_for_randint():
-    path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
     with open(path, 'r') as content_file:
         content = content_file.read()
-        regex = re.compile(r"random\.randint\s*\(\s*\d+\s*,\s*\d+\s*\)")
+        regex = re.compile(r"random\.randint\s*\(\s*1\s*,\s*100\s*\)")
         assert bool(regex.search(content)) == True
 
-
-@pytest.mark.it("The list my_list should have 15 items in the end")
+@pytest.mark.it("The list my_list should have 10 items in the end")
 def test_for_size():
     from app import my_list
-    assert len(my_list) == 15
+    assert len(my_list) == 10
+
+@pytest.mark.it("The original my_list should not have any values hardcoded into it")
+def test_no_hardcoding():
+    with open(path, 'r') as content_file:
+        content = content_file.read()
+        # Assuming the original list is provided as 'my_list = [4,5,734,43,45]'
+        # Counting the occurrences of this exact string ensures it hasn't been modified.
+        occurrences = content.count('my_list = [4,5,734,43,45]')
+        assert occurrences == 1
