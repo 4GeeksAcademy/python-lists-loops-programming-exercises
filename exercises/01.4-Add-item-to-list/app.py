f92e5d3782643ea6b148bb9e4db8661faf3e3634
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-16 05:34:04 +0000

    edited cues

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,9 +1,10 @@
 # ✅ ↓ Remember import random function here: ↓ ✅
 
-# ❌ ↓  Do NOT change the line below ↓ ❌
+# ❌ ↓  Do NOT change the line of code below ↓ ❌
 
 my_list = [4,5,734,43,45]
 
-# ❌ ↑ Do NOT change the line above ↑ ❌
+# ❌ ↑ Do NOT change the line of code above ↑ ❌
 
 # ✅ ↓ The magic is here: ↓ ✅
+
