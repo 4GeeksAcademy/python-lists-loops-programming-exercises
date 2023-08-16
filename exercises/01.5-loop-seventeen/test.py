1c5275200123abd916c166983ecb80ad32745f17
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-16 06:05:01 +0000

    added hardcode check

--- exercises/01.5-loop-seventeen/test.py
+++ exercises/01.5-loop-seventeen/test.py
@@ -9,12 +9,20 @@ def test_for_loop():
         regex = re.compile(r"for(\s)+[a-zA-Z\-_]+(\s)+in(\s)+range.*")
         assert bool(regex.search(content)) == True
 
+@pytest.mark.it("You should not be hardcoding the answer")
+def test_hard_code():
+    with open(path, 'r') as content_file:
+        content = content_file.read()
+        regex = re.compile(r"\"\s*1\\n2\\n3\\n4\\n5\\n6\\n7\\n8\\n9\\n10\\n11\\n12\\n13\\n14\\n15\\n16\\n17\\n\s*\"")
+        assert bool(regex.search(content)) == False
+
 @pytest.mark.it("Use the function print once inside your loop")
 def test_use_print():
     with open(path, 'r') as content_file:
         content = content_file.read()
         regex = re.compile(r"print(\s)*\(")
         assert bool(regex.search(content)) == True
+
 
 @pytest.mark.it("Print on the console from 1 to 17 (do not print 0)")
 def test_output(capsys, app):
