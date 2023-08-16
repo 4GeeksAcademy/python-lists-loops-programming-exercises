b15033f5035eb0ad4c57e2e6db527cd75b66b41f
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-16 04:38:59 +0000

    added additional visual cues

--- exercises/01.3-Print-the-last-one/app.py
+++ exercises/01.3-Print-the-last-one/app.py
@@ -1,6 +1,7 @@
 #  import the random package here ✅ ↓ "import random" ↓ ✅
 
 
+# ❌ ↓  Do NOT change the code below this line ↓ ❌
 def generate_random_list():
     aux_list = []
     randonlength = random.randint(1, 100)
@@ -9,6 +10,8 @@ def generate_random_list():
         aux_list.append(randonlength)
         i += i
     return aux_list
-my_stupid_list = generate_random_list()
+my_stupid_list = generate_random_list() 
+
+# ❌ ↑ Do NOT change the code above this line ↑ ❌
 
 # ✅ ↓ Feel happy to write the code below this comment, good luck!: ↓ ✅
