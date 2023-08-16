646a848c426e9ee74359c4c276737dfcd62ae55f
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-16 05:34:40 +0000

    added solution

---
+++ exercises/01.4-Add-item-to-list/solution.hide.py
@@ -0,0 +1,16 @@
+# ✅ ↓ Remember import random function here: ↓ ✅
+import random
+
+# ❌ ↓  Do NOT change the line of code below ↓ ❌
+
+my_list = [4,5,734,43,45]
+
+# ❌ ↑ Do NOT change the line of code above ↑ ❌
+
+# ✅ ↓ The magic is here: ↓ ✅
+
+my_list.append(random.randint(1,100))
+my_list.append(random.randint(1,100))
+my_list.append(random.randint(1,100))
+my_list.append(random.randint(1,100))
+my_list.append(random.randint(1,100))
