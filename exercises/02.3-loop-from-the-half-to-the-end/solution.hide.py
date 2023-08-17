b2a5d97af1246937b959a989a8fe8a3031ba3d8b
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-17 06:11:41 +0000

    added notes

--- exercises/02.3-loop-from-the-half-to-the-end/solution.hide.py
+++ exercises/02.3-loop-from-the-half-to-the-end/solution.hide.py
@@ -3,7 +3,7 @@ my_list = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12]
 # ❌ ↑ Do NOT change the line of code above ↑ ❌
 
 # ✅ ↓ Your code goes here: ↓ ✅
-initialValue = int(len(my_list) / 2) 
+initialValue = int(len(my_list) / 2) # ⚠️ When we devide, we get a float (decimal number) back, that's why we are coverting it to int ⚠️
 stopValue = len(my_list)
 increaseValue = 1
 
