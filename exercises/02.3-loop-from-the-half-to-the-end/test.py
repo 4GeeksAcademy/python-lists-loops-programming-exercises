161b72fd3146ff171cb3dc0f055dc743e1e042a3
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-17 06:07:45 +0000

    added harcode check

--- exercises/02.3-loop-from-the-half-to-the-end/test.py
+++ exercises/02.3-loop-from-the-half-to-the-end/test.py
@@ -8,6 +8,13 @@ def test_half_to_end(capsys, app):
     captured = capsys.readouterr()
     assert "23\n48\n56432\n55\n23\n25\n12\n" in captured.out
 
+@pytest.mark.it("You should not be hardcoding the answer")
+def test_hard_code():
+    with open(path, 'r') as content_file:
+        content = content_file.read()
+        regex = re.compile(r"\"\s*23\\n48\\n56432\\n55\\n23\\n25\\n12\\n\s*\"")
+        assert bool(regex.search(content)) == False
+
 @pytest.mark.it("Use the for loop")
 def test_for_loop():
     with open(path, 'r') as content_file:
