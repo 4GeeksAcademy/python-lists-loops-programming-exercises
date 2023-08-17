5eda21602e645322ba0bf0bd97d29258fababbc8
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-17 05:57:15 +0000

    added visual cues

--- exercises/02.3-loop-from-the-half-to-the-end/app.py
+++ exercises/02.3-loop-from-the-half-to-the-end/app.py
@@ -1,9 +1,13 @@
 my_list = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12]
 
-#Your code here:
+# ❌ ↑ Do NOT change the line of code above ↑ ❌
+
+# ✅ ↓ Your code goes here: ↓ ✅
 inicialValue = 0
 stopValue = 0
 increaseValue = 0
+
+# ❌ ↓ Do NOT change any code below this line ↓ ❌
 
 for i in range(inicialValue, stopValue):
     if i == my_list[i]:
