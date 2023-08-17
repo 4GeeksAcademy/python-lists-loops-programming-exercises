adb629bd6c0c847b48c523bc65f66656b36ca0fb
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-17 05:37:01 +0000

    added visual cues

--- exercises/02.2-Loop-adding-two/app.py
+++ exercises/02.2-Loop-adding-two/app.py
@@ -1,7 +1,8 @@
 my_sample_list= [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12]
 
-#your code go below of this line, nothing change above:
+# ❌ ↑ Do NOT change the line of code above ↑ ❌
 
+# ✅ ↓ Your code goes here: ↓ ✅
 
 for i in range(0, len(my_sample_list), 1):
     print(my_sample_list[i])
