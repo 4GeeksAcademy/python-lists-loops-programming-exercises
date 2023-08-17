7f7f065f3e0409c451f0aa97428599e6ee16a366
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-17 04:59:13 +0000

    added hardcode check

--- exercises/02.1-Loop-from-the-top/test.py
+++ exercises/02.1-Loop-from-the-top/test.py
@@ -6,6 +6,14 @@ def test_output(capsys, app):
     app()
     captured = capsys.readouterr()
     assert "12\n25\n23\n55\n56432\n48\n23\n867543\n8\n654\n47889\n4\n5\n3423\n" in captured.out
+    
+@pytest.mark.it("You should not be hardcoding the answer")
+def test_hard_code():
+    with open(path, 'r') as content_file:
+        content = content_file.read()
+        regex = re.compile(r"\"\s*12\\n25\\n23\\n55\\n56432\\n48\\n23\\n867543\\n8\\n654\\n47889\\n4\\n5\\n3423\\n\s*\"")
+        assert bool(regex.search(content)) == False
+
 
 @pytest.mark.it("Use the while loop")
 def test_for_loop():
