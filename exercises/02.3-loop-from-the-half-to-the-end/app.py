2d492670fb5616761532a6d13995217c543a55e5
Ernesto Gonzalez <60261375+UmiKami@users.noreply.github.com>
2023-08-17 05:59:28 +0000

    fixed typo

--- exercises/02.3-loop-from-the-half-to-the-end/app.py
+++ exercises/02.3-loop-from-the-half-to-the-end/app.py
@@ -3,13 +3,13 @@ my_list = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12]
 # ❌ ↑ Do NOT change the line of code above ↑ ❌
 
 # ✅ ↓ Your code goes here: ↓ ✅
-inicialValue = 0
+initialValue = 0
 stopValue = 0
 increaseValue = 0
 
 # ❌ ↓ Do NOT change any code below this line ↓ ❌
 
-for i in range(inicialValue, stopValue):
+for i in range(initialValue, stopValue):
     if i == my_list[i]:
         i += increaseValue
     print(my_list[i])
