bf88f57c8ea67997ddce2a183564dd6c5e0ca7df
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-29 15:36:36 +0100

    Create solution.hide.py

---
+++ exercises/02.3-loop-from-the-half-to-the-end/solution.hide.py
@@ -0,0 +1,11 @@
+my_list = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12]
+
+# Your code here
+inicial_value = 0
+stop_value = 0
+increase_value = 0
+
+for i in range(inicial_value, stop_value):
+    if i == my_list[i]:
+        i += increase_value
+    print(my_list[i])
