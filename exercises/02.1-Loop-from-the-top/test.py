2f254126a8b2f6a67e9e34c4a9294bc66c4abb67
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-29 14:55:12 +0100

    Update test.py

--- exercises/02.1-Loop-from-the-top/test.py
+++ exercises/02.1-Loop-from-the-top/test.py
@@ -7,9 +7,9 @@ def test_output(capsys, app):
     captured = capsys.readouterr()
     assert "12\n25\n23\n55\n56432\n48\n23\n867543\n8\n654\n47889\n4\n5\n3423\n" in captured.out
 
-@pytest.mark.it("Use the while loop")
+@pytest.mark.it("Use the for loop")
 def test_for_loop():
     with open(path, 'r') as content_file:
         content = content_file.read()
-        regex = re.compile(r"while(\s)+[a-zA-Z\-_]+(\s)")
+        regex = re.compile(r"for(\s)+[a-zA-Z\-_]+(\s)")
         assert bool(regex.search(content)) == True
