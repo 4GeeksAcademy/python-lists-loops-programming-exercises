820dc2d15c89cd4057412c5e39ac0e394e5a97e4
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-29 04:52:41 +0100

    Update test.py

--- exercises/01.1-Access-and-Retrieve/test.py
+++ exercises/01.1-Access-and-Retrieve/test.py
@@ -1,18 +1,18 @@
 import io, sys, pytest
 
-@pytest.mark.it('Your console have to print the 3rd item from the `list`')
+@pytest.mark.it('Your console has to print the 3rd item from my_list')
 def test_output_one(capsys, app):
     app()
     captured = capsys.readouterr()
     assert "tuesday\n" in captured.out
 
-@pytest.mark.it('Your code have to print the position of step 2')
+@pytest.mark.it('Your code has to print the fourth position from my_list')
 def test_output_two(capsys, app):
     app()
     captured = capsys.readouterr()
     assert "None\n" in captured.out
 
-@pytest.mark.it('Set the position were thrusday is to None')
+@pytest.mark.it('Set the value of thursday to None')
 def test_position_two():
     from app import my_list
     assert my_list[4] is None
