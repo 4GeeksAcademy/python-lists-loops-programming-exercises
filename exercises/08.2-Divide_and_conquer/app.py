0ae7ee2de5103c1f76922d5004df936e8273d75d
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-29 19:45:57 +0100

    Update app.py

--- exercises/08.2-Divide_and_conquer/app.py
+++ exercises/08.2-Divide_and_conquer/app.py
@@ -1,8 +1,7 @@
-list_of_numbers = [4,	80,	85,	59,	37, 25, 5, 64, 66, 81, 20, 64, 41, 22, 76, 76, 55, 96, 2, 68]
+list_of_numbers = [4, 80, 85, 59, 37, 25, 5, 64, 66, 81, 20, 64, 41, 22, 76, 76, 55, 96, 2, 68]
+
+# Your code here
 
 
-#Your code here:
-
-
-print(merge_two_list(list_of_numbers))
+print(sort_odd_even(list_of_numbers))
 
