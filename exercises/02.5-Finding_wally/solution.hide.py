ef76a97a4670f18b69af8fd9d0e93c12d00ff5b4
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-29 20:58:20 +0100

    Update solution.hide.py

--- exercises/02.5-Finding_wally/solution.hide.py
+++ exercises/02.5-Finding_wally/solution.hide.py
@@ -3,5 +3,5 @@ people = ['Lebron','Aaliyah','Diamond','Dominique','Aliyah','Jazmin','Darnell','
 # Your code here
 
 for i in range(0, len(people)):
-    if people[i] == "Wally":
+    if people[i].lower() == "wally":
         print(i)
