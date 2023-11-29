f622e3809ed01f8c4801d6d36c17d9dd72e813da
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-29 05:39:04 +0100

    Update app.py

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,5 @@
-#Remember import random function here:
+# Remember to import random function here
 
-my_list = [4,5,734,43,45]
+my_list = [4, 5, 734, 43, 45]
 
-#The magic is here:
+# The magic goes below
