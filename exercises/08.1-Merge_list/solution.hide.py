9deae3119f3cdbd9415b96d52797f372c2c2245f
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-29 19:15:24 +0100

    Create solution.hide.py

---
+++ exercises/08.1-Merge_list/solution.hide.py
@@ -0,0 +1,16 @@
+chunk_one = ['Lebron', 'Aaliyah', 'Diamond', 'Dominique', 'Aliyah', 'Jazmin', 'Darnell']
+chunk_two = ['Lucas', 'Jake', 'Scott', 'Amy', 'Molly', 'Hannah', 'Lucas']
+
+
+def merge_list(list1, list2):
+    # Your code here
+    new_list = []
+    for i in list1:
+        new_list.append(i)
+    for i in list2:
+        new_list.append(i)
+    
+    return new_list
+    
+    
+print(merge_list(chunk_one, chunk_two))
