58997200c60f72e5725af357bd243058ada19a8c
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-29 15:23:44 +0100

    Update app.py

--- exercises/02.2-Loop-adding-two/app.py
+++ exercises/02.2-Loop-adding-two/app.py
@@ -1,4 +1,4 @@
-my_sample_list= [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12]
+my_sample_list = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12]
 
 # Your code below, don't change anything above
 
