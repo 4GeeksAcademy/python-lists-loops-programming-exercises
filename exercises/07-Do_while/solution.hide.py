6ccd53f08f48a7c497893f10e3baf179dc767ba1
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-11-29 18:21:06 +0100

    Create solution.hide.py

---
+++ exercises/07-Do_while/solution.hide.py
@@ -0,0 +1,12 @@
+# Your code here
+
+number = 20
+
+while number >= 1:
+    if number % 5 == 0:
+        print(str(number) + "!")
+    else:
+        print(number)
+    number -= 1
+
+print("LIFTOFF")
