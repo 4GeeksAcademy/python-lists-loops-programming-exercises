c3c0cff3ed907b75a5d108ed286c33a57e57ffcd
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-05 23:50:27 +0100

    Update app.py

--- exercises/12-Map_a_list/app.py
+++ exercises/12-Map_a_list/app.py
@@ -1,9 +1,8 @@
-Celsius_values = [-2,34,56,-10]
-
-
+celsius_values = [-2, 34, 56, -10]
 
 def fahrenheit_values(x):
-# the magic go here:
+   # The magic happens here
    
-result = list(map(fahrenheit_values, Celsius_values))
+   
+result = list(map(fahrenheit_values, celsius_values))
 print(result)
