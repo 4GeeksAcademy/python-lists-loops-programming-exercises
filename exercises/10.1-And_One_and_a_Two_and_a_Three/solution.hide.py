75f6eee42c2a5b9603ee58e537980e935d6fbd4d
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-05 22:19:14 +0100

    Create solution.hide.py

---
+++ exercises/10.1-And_One_and_a_Two_and_a_Three/solution.hide.py
@@ -0,0 +1,10 @@
+contact = {
+    "fullname": "Jane Doe",
+    "phone": "321-321-4321",
+    "email": "test@test.com"
+}
+
+# Your code here
+
+for key in contact.keys():
+    print(f"{key}: {contact[key]}")
