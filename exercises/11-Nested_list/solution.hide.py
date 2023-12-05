b49b78b9544a8d471c7a18e53e0caccb20e50c62
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-05 23:16:20 +0100

    Create solution.hide.py

---
+++ exercises/11-Nested_list/solution.hide.py
@@ -0,0 +1,11 @@
+coordinates_list = [[33.747252, -112.633853], [-33.867886, -63.987], [41.303921, -81.901693], [-33.350534, -71.653268]]
+
+# Your code here
+
+longitudes = []
+
+for coord in coordinates_list:
+    longitudes.append(coord[1])
+
+for lon in longitudes:
+    print(lon)
