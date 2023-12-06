88836b78a655f0741b13c678a27899518d2abc11
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-06 16:03:08 +0100

    Update test.py

--- exercises/14-Loop-dictionary/test.py
+++ exercises/14-Loop-dictionary/test.py
@@ -1,28 +1,28 @@
 import io, sys, os, pytest, re
 path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
 
-@pytest.mark.it("You should add `love` programatically to the dictionary")
+@pytest.mark.it("You should add `love` programmatically to the dictionary")
 def test_love():
     f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
     content = f.read()
     regex = re.compile(r"spanish_translations\[\s*[\'\"]love[\'\"]\s*\]\s*\=\s*[\'\"]amor[\'\"]")
     assert bool(regex.search(content)) == True
 
-@pytest.mark.it("You should add `code` programatically to the dictionary")
+@pytest.mark.it("You should add `code` programmatically to the dictionary")
 def test_code():
     f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
     content = f.read()
     regex = re.compile(r"spanish_translations\[\s*[\'\"]code[\'\"]\s*\]\s*\=\s*[\'\"]codigo[\'\"]")
     assert bool(regex.search(content)) == True
 
-@pytest.mark.it("You should add `smart` programatically to the dictionary")
+@pytest.mark.it("You should add `smart` programmatically to the dictionary")
 def test_smart():
     f = open(os.path.dirname(os.path.abspath(__file__))+'/app.py')
     content = f.read()
     regex = re.compile(r"spanish_translations\[\s*[\'\"]smart[\'\"]\s*\]\s*\=\s*[\'\"]inteligente[\'\"]")
     assert bool(regex.search(content)) == True
 
-@pytest.mark.it("You should print in the console the dictionary with proper translations")
+@pytest.mark.it("You should print in the console the dictionary with the proper translations")
 def test_multp(capsys, app):
     import app
     captured = capsys.readouterr()
