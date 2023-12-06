fa285a6ce6173b64678e98d97cc7e0ae9441bbe7
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-06 11:47:11 +0100

    Create solution.hide.py

---
+++ exercises/12.2-Map_function_inside_variable/solution.hide.py
@@ -0,0 +1,10 @@
+names = ['Alice', 'Bob', 'Marry', 'Joe', 'Hilary', 'Stevia', 'Dylan']
+
+def prepender(name):
+    return "My name is: " + name
+    
+# Your code here
+
+new_list = list(map(prepender, names))
+
+print(new_list)
