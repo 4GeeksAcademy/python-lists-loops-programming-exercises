b5149ae7f52a1c8d1e1247c7d3707d375801ab88
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-06 13:29:13 +0100

    Update test.py

--- exercises/12.5-Yes_and_no/test.py
+++ exercises/12.5-Yes_and_no/test.py
@@ -1,19 +1,19 @@
 import io, sys, pytest, os, re
 path = os.path.dirname(os.path.abspath(__file__))+'/app.py'
 
-@pytest.mark.it("Congratulation!!!, that was a nice work")
+@pytest.mark.it("The output should be correct")
 def test_output(capsys, app):
     import app
     captured = capsys.readouterr()
     assert "['woko', 'wiki', 'woko', 'woko', 'wiki', 'wiki', 'wiki', 'woko', 'woko', 'wiki', 'woko', 'wiki', 'wiki', 'woko', 'woko', 'woko', 'woko', 'woko', 'woko', 'woko', 'woko', 'wiki', 'woko', 'woko', 'woko', 'woko', 'wiki']\n" in captured.out
 
-@pytest.mark.it("Use a map function")
+@pytest.mark.it("Use the map function")
 def test_map():
     f = open(os.path.dirname(os.path.abspath(__file__)) + '/app.py')
     content = f.read()
     assert content.find("map") > 0
 
-@pytest.mark.it("Use if statement")
+@pytest.mark.it("Use an if statement")
 def test_if():
     with open(path, 'r') as content_file:
         content = content_file.read()
