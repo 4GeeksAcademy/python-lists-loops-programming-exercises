a2ff36d289b136154e6c0d228ff90ee1a40f7533
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-06 13:41:02 +0100

    Update solution.hide.py

--- exercises/12.5-Yes_and_no/solution.hide.py
+++ exercises/12.5-Yes_and_no/solution.hide.py
@@ -2,6 +2,6 @@ the_bools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1]
 
 # Your code here
 
-result = list(map(lambda x: 'wiki' if x == 1 else 'woko', the_bools))
+new_list = list(map(lambda x: 'wiki' if x == 1 else 'woko', the_bools))
 
-print(result)
+print(new_list)
