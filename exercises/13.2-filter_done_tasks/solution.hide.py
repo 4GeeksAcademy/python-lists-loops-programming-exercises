99fa73bb72c27637eade6c222ab21f9c30d01d4e
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-06 14:37:46 +0100

    Create solution.hide.py

---
+++ exercises/13.2-filter_done_tasks/solution.hide.py
@@ -0,0 +1,19 @@
+tasks = [
+	{ "label": 'Eat my lunch', "done": True },
+	{ "label": 'Make the bed', "done": False },
+	{ "label": 'Have some fun', "done": False },
+	{ "label": 'Finish the replits', "done": False },
+	{ "label": 'Replit the finishes', "done": True },
+	{ "label": 'Ask for a raise', "done": False },
+	{ "label": 'Read a book', "done": True },
+	{ "label": 'Make a trip', "done": False }
+]
+
+
+# Your code here
+def filter_done_tasks(task):
+    return task["done"]
+
+done_tasks = list(filter(filter_done_tasks, tasks))
+
+print(done_tasks)
