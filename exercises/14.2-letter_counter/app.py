bd8bbad0894a1384480d66b09a786b3c866c2222
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-06 16:17:57 +0100

    Update app.py

--- exercises/14.2-letter_counter/app.py
+++ exercises/14.2-letter_counter/app.py
@@ -1,7 +1,6 @@
 par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 
 counts = {}
-#your code go here:
+# Your code here
 
 print(counts)
-
