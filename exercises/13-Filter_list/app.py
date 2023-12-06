186ae6f5db75ca0239c04bffc8808a72e6dd36b5
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-06 13:54:05 +0100

    Update app.py

--- exercises/13-Filter_list/app.py
+++ exercises/13-Filter_list/app.py
@@ -1,9 +1,9 @@
 all_numbers = [23,12,35,5,3,2,3,54,3,21,534,23,42,1]
 
 
-def filter_function(items):
+def filter_function(item):
     # Update here
-    return items % 2 == 0
+    return item % 2 == 0
     
 greater_than_ten = list(filter(filter_function, all_numbers))
 
