bbabdf88b342468dd2b026a1b0512f696b1bdca3
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-06 12:02:55 +0100

    Update app.py

--- exercises/12.3-Map_data_types/app.py
+++ exercises/12.3-Map_data_types/app.py
@@ -1,8 +1,9 @@
-list_Strings = ['1','5','45','34','343','34',6556,323]
-
+mixed_list = ['1','5','45','34','343','34',6556,323]
 
 def type_list(items):
-        return items
+        # Your code here
+        return 
 
-new_list = list(map(type_list, list_Strings))
+new_list = list(map(type_list, mixed_list))
+
 print(new_list)
