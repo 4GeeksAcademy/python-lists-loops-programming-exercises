b18ee898a0761bdc170124abc32d7c62d0e189b2
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-06 18:03:43 +0100

    Update app.py

--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -1,6 +1,6 @@
 
 
-# Your code go above, nothing to change after this line:
+# Your code above, nothing to change after this line
 print(lyrics_generator([0,0,1,1,0,0,0]))
 print(lyrics_generator([0,0,1,1,1,0,0,0]))
 print(lyrics_generator([0,0,0]))
