a0b3ea7dfc47785d572c7c41c099c8e7626f582b
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-12-06 14:10:07 +0100

    Update app.py

--- exercises/13.1-Filter_and_list/app.py
+++ exercises/13.1-Filter_and_list/app.py
@@ -1,7 +1,6 @@
+all_names = ["Romario", "Boby", "Roosevelt", "Emiliy", "Michael", "Greta", "Patricia", "Danzalee"]
 
-all_names = ["Romario","Boby","Roosevelt","Emiliy", "Michael", "Greta", "Patricia", "Danzalee"]
-
-#Your code go here:
+# Your code here
 
 print(resulting_names)
 
