6d7d8f2579b53a78a7f6a61348824852140eb076
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-11-11 01:25:55 +0000

    looping beginners excercises done

--- exercises/02.4-One_last_looping/app.py
+++ exercises/02.4-One_last_looping/app.py
@@ -1,3 +1,8 @@
 names = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','Fernando','Cesco','Bart','Annie']
 
 # Your code here
+names[1]= "Steven"
+names[-1]= "Pepe"
+names[0]=names[2]+names[4]
+for i in range(len(names)):
+    print(names[-1-i])
