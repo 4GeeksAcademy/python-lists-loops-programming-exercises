ea514a9115db073a964204a7eb41f4b59c5d4062
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-11-13 03:01:14 +0000

    Done till 13.4, tomorrow we will start with 14

--- .learn/resets/13.3-Filter_list_strings/app.py
+++ .learn/resets/13.3-Filter_list_strings/app.py
@@ -1,7 +1,3 @@
 names = ['Liam','Emma','Noah','Olivia','William','Ava','James','Isabella','Logan','Sophia','Benjamin','Mia','Mason','Charlotte','Elijah','Amelia','Oliver','Evelyn','Jacob','Abigail','Lucas','Harper','Michael','Emily','Alexander','Elizabeth','Ethan','Avery','Daniel','Sofia','Matthew','Ella','Aiden','Madison','Henry','Scarlett','Joseph','Victoria','Jackson','Aria','Samuel','Grace','Sebastian','Chloe','David','Camila','Carter','Penelope','Wyatt','Riley']
 
 # Your code here
-def am_in_name(name):
-    if "am" in name or "Am" in name:
-        return name
-print(list(filter(am_in_name,names)))
